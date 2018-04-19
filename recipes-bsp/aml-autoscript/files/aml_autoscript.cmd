6895d6d1f81bc025cf8e4eff2aa07f11a71cbfd6
Kevin Hilman <khilman@baylibre.com>
2018-04-19 14:07:45 -0700

    recipes-bsp/aml-autoscript: wait for MMC root
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- recipes-bsp/aml-autoscript/files/aml_autoscript.cmd
+++ recipes-bsp/aml-autoscript/files/aml_autoscript.cmd
@@ -1,6 +1,6 @@
 setenv kernel_loadaddr "0x11000000"
 setenv initrd_loadaddr "0x13000000"
 setenv dtb_mem_addr "0x1000000"
-setenv bootargs "console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 root=/dev/mmcblk1p2"
+setenv bootargs "console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 root=/dev/mmcblk1p2 rootwait"
 if fatload mmc 0 ${kernel_loadaddr} uImage; then if fatload mmc 0 ${dtb_mem_addr} ##DTB## ; then bootm ${kernel_loadaddr} - ${dtb_mem_addr} ; fi ; fi
 if fatload mmc 1 ${kernel_loadaddr} uImage; then if fatload mmc 1 ${dtb_mem_addr} ##DTB## ; then bootm ${kernel_loadaddr} - ${dtb_mem_addr} ; fi ; fi
